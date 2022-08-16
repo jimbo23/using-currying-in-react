2cd17d99527c3cefb5ad13513401d3dd94bd176c
jimbo23 <kiefersoon@gmail.com>
2022-08-16 14:51:48 +0800

    Currying example 2

--- src/Example2.js
+++ src/Example2.js
@@ -1,15 +1,34 @@
 import React from 'react';
 
+/**
+ * Consider a scenario where you need some data for analytics
+ * You need to tell which button / any other HTML elements are clicked
+ */
+
 const Example2 = () => {
-  const handleClick = (arg) => (e) => {
-    console.log(`${e.target.innerHTML} ${arg}`);
+  /** Instead of doing this: 👇🏼❌ 
+    const handleClick1 = (e) => {
+      console.log('button 1 is clicked')
+    } 
+
+    const handleClick1 = (e) => {
+      console.log('button 2 is clicked')
+    } 
+   */
+
+  /** Do this: 👇🏼✅ */
+  const handleClick = (buttonId) => (e) => {
+    console.log(`${e.target.innerHTML} ${buttonId} is clicked`);
   };
 
   return (
     <>
       <strong>Example2: </strong>
       <div>
-        <button onClick={handleClick('was clicked')}>Press me</button>
+        <button onClick={handleClick(1)}>Button</button>
+      </div>
+      <div>
+        <button onClick={handleClick(2)}>Button</button>
       </div>
     </>
   );
